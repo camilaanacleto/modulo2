cce2e9dc3365d4aa2c41b6545420f80977793fa7
camilaanacleto <68927480+camilaanacleto@users.noreply.github.com>
2022-05-30 19:38:30 -0300

    ex1

---
+++ 03_AUT_EST_ENTREGA/Semana 3/ex/code.js
@@ -0,0 +1,7 @@
+function calculo()
+{
+  var v0 = parseInt(document.getElementById('v0').value);
+  var tempo = v0 / 10; 
+  var hmax = v0^2/2 * 10; 
+  alert("A altura máxima é de " + hmax +" metros. O tempo total é de " + tempo +" segundos"); 
+}
