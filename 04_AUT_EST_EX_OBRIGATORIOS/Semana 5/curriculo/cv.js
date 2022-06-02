2a0bfc5bb97b5ea8e54a6eebc373f3ff7094592f
camilaanacleto <68927480+camilaanacleto@users.noreply.github.com>
2022-06-02 00:12:08 -0300

    atualização de pastas

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/curriculo/cv.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/curriculo/cv.js
@@ -3,3 +3,5 @@ const btn = document.getElementById("btn");
 btn.addEventListener("change", (e) => {
     document.body.classList.toggle("dark", e.target.checked);
 })
+
+$('h1').fadeOut("2000").fadeIn("2000")
